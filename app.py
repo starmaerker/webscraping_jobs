165971147b9d0e627752b26278fd4e85fbe3247e
Andreas Gropp <starmaerker@googlemail.com>
2020-05-03 15:55:18 +0200

    update app.py

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(soup)
+results = soup.find(id='SearchResults')
+
+jobs = results.find_all('section', class_='card-content')
+
+counter = 1
+
+for job in jobs:
+    title_elem = job.find('h2', class_='title')
+    company_elem = job.find('div', class_='company')
+    location_elem = job.find('div', class_='location')
+    print(str(counter) + ': ')
+    if title_elem is not None:
+        print(title_elem.text)
+    if company_elem is not None:
+        print(company_elem.text)
+    if location_elem is not None:
+        print(location_elem.text)
+    print()
+    counter += 1
+
+
+
+
 
